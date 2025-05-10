368902c9cb8460f38f8757ea3cc2287271a86cdc
JuliaGalich <julia.galich2011@gmail.com>
2025-05-10 14:51:03 +0300

    added image

--- app/forms.py
+++ app/forms.py
@@ -4,3 +4,7 @@ class PromptForm(forms.Form):
     prompt = forms.CharField(label = 'Enter your prompt:',
                              max_length = 500,
                              widget=forms.Textarea)
+
+# class CustomUserCreationForm(UserCreationForm):
+#     class Meta:
+#         fields = ['']
--- app/views.py
+++ app/views.py
@@ -1,26 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import PromptForm
 from project.settings import API_KEY
+from django.contrib.auth.forms import UserCreationForm
 
 import openai
 #openai.api_key = ''
 client = openai.OpenAI(api_key=API_KEY)
 
 def home(request):
-    response_text = None
-    response_image = None
+    if request.user.is_autheticated:
+        QueryHistiry.objects.create(
+            user = request.user,
+            query = query,
+            response_text=None,
+            response_image = None
+        )
+
 
     if request.method == 'POST':
         form = PromptForm(request.POST)
-
-        want_text = 'text_response' in request.POST
-        want_image = 'image_response' in request.POST
-
         # is_valid
         if form.is_valid():
             prompt = form.cleaned_data['prompt']
 
-            want_text = client.chat.completions.create(
+            completion = client.chat.completions.create(
                 model="gpt-4.1",
                 messages=[
                     {"role": "developer", "content": "You are a helpful assistant."},
@@ -28,15 +31,13 @@ def home(request):
                 ]
             )
 
-            response_text = want_image.choices[0].message.content
+            response_text = completion.choices[0].message.content
 
-            if want_image:
-                image_result = client.images.generate(
-                    model="dall-e-3",
-                    prompt=prompt,
-                    size="512x512",
-                    response_format="url"
-                )
+            image_result = client.images.generate(
+                model="dall-e-3",
+                prompt=prompt
+            )
+
             response_image = image_result.data[0].url
 
             #model "gpt-image-1"
@@ -44,13 +45,13 @@ def home(request):
             #     model="gpt-image-1",
             #     prompt=prompt
             # )
-            #
+
             # image_base64 = image_result.data[0].b64_json
             # response_image = image_base64.b64decode(image_base64)
 
             # Save the image to a file
-            with open("otter.png", "wb") as f:
-                f.write(response_image)
+            # with open("otter.png", "wb") as f:
+            #     f.write(response_image)
 
     else:
         form = PromptForm()
@@ -60,3 +61,14 @@ def home(request):
         'response_text':response_text,
         'response_image': response_image
     })
+
+
+def singup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
